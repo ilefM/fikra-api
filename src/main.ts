4114b73295ed9deab3a1c62568b23575ef691e64
ilefM <ilefikhelefdev1@gmail.com>
2023-11-04 11:52:37 -0400

    cors options

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const whitelist = ['http://localhost:5173'];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-  app.enableCors();
+  app.enableCors({
+    origin: whitelist,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
