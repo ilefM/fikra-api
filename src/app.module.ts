109770749cb8ee2d2e333eeae16a6f9f17a6e032
ilefM <ilefikhelefdev1@gmail.com>
2024-01-05 11:58:54 -0500

    changing env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import { PostModule } from './post/post.module';
 import { PrismaModule } from './prisma/prisma.module';
 
+const ENV = process.env.NODE_ENV;
+
+console.log(ENV);
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, PostModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ENV ? '.env.development' : `.env.${ENV}`,
+    }),
+    PrismaModule,
+    PostModule,
+  ],
 })
 export class AppModule {}
