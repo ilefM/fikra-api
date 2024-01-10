2c740449788b09cf81099f621017217ea6526e36
ilefM <ilefikhelefdev1@gmail.com>
2024-01-10 17:55:59 -0500

    Deployement setup (#11)
    * changing env variables
    
    * delete configuration file
    
    * CD yaml

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { PostModule } from './post/post.module';
 import { PrismaModule } from './prisma/prisma.module';
 
+const ENV = process.env.NODE_ENV;
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, PostModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ENV ? '.env.development' : `.env.${ENV}`,
+    }),
+    PrismaModule,
+    PostModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-const whitelist = ['http://localhost:5173'];
+const whitelist = ['http://localhost:5173', 'https://fikra.vercel.app/'];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
