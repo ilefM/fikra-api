2d46823ea5507a7b86c4bfed82f9e1caddb8855c
ilefM <ilefikhelefdev1@gmail.com>
2024-01-11 13:58:55 -0500

    removing unused variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { PostModule } from './post/post.module';
 import { PrismaModule } from './prisma/prisma.module';
 
-const ENV = process.env.NODE_ENV;
-
 @Module({
   imports: [
     ConfigModule.forRoot({
