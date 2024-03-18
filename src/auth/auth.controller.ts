864c934df4c0e6b6370925d39afa25f204f3a906
ilefM <ilefikhelefdev1@gmail.com>
2024-03-17 21:50:26 -0400

    setup of auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, Post, Body, Delete } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  signUp() {}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
 
 const whitelist = ['http://localhost:5173', 'https://fikra.vercel.app'];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 60000 },
+    }),
+  );
+
   app.enableCors({
     origin: whitelist,
   });
